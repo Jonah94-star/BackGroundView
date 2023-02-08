1dc0192419bb05beef3a5241eebc7d774ab8bbe2
Jonah94 <124059065+Jonah94-star@users.noreply.github.com>
2023-02-08 23:22:43 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/backgroundview/MainActivity.java
+++ app/src/main/java/com/example/backgroundview/MainActivity.java
@@ -11,7 +11,7 @@ import com.example.backgroundview.service.BroadcastReceiverService;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
